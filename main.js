81f67c306eea9bfc4be126744210b6efe0155d8d
LucasLimaDM <lucaslim.dias@gmail.com>
2023-01-18 19:36:31 -0300

    adição a array de favoritos sem resposta visual

--- main.js
+++ main.js
@@ -25,26 +25,54 @@ form.addEventListener('submit', function(e) {
 
 
 function atualizaTabela() {
+    console.log('atualiza tabela on')
     tableBody.innerHTML = ''
-    for (let i = 0; i < contatos.length; i++) {
-        tableBody.innerHTML += `<tr><td>${contatos[i]}</td> <td>${numeros[i]}</td><td><ion-icon name="star-outline" class="star-icon" id="star${i}" onclick="pintaEstrela(this)"></ion-icon></td></tr>`
-        console.log(i)
+    for (let i = 0; i < favContatos.length; i++) {
+        tableBody.innerHTML += `<tr><td>${favContatos[i]}</td> <td>${favNumeros[i]}</td><td><ion-icon name="star" style= "color: #ffd000;" class="star-icon" id="star-${i}" onclick="pintaEstrela(this)"></ion-icon></td></tr>`
+        console.log('for FavContatos = ' + i)
     }
-
+    for (let i = 0; i < contatos.length; i++) {
+        tableBody.innerHTML += `<tr><td>${contatos[i]}</td> <td>${numeros[i]}</td><td><ion-icon name="star-outline" class="star-icon" id="star-${favContatos.length + i}" onclick="pintaEstrela(this)"></ion-icon></td></tr>`
+        console.log('for contatos comuns = ' + i)
+    }
 
     
 }
 
 
 function pintaEstrela(element) {
+
     if (element.name == 'star-outline') {
-        element.style.color = '#ffd000'
+        
         element.name = 'star'
+        var starPosition = element.id.split('-').slice(1)[0]
+        console.log(starPosition)
+        favContatos.push(contatos.splice(starPosition, 1)[0])
+        favNumeros.push(numeros.splice(starPosition, 1)[0])
+        console.log('push dos dados')
+        console.log(favContatos)
+        console.log(favNumeros)
+        console.log(contatos)
+        console.log(numeros)
+       
+        
+        
     } else {
         element.style.color = 'black'
         element.name = 'star-outline'
-    }
+        contatos.push(favContatos.splice(starPosition, 1))
+        numeros.push(favNumeros.splice(starPosition, 1))
+        
+        console.log('dados removidos dos favoritos')
+        console.log(favContatos)
+        console.log(favNumeros)
+        console.log(contatos)
+        console.log(numeros)
 
+    }
+    atualizaTabela()
+    element.style.color = '#ffd000'
+    console.log('click resolvido')
 }
 
 
